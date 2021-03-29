08707589e307bf8f18fd4c161e0c3856015dfe78
SoumyaDarshanPadhy <soumyadarshanpadhy@gmail.com>
2021-03-30 01:17:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require("socket.io")(httpServer, {
+    cors: {
+        origin: "https://secret-chat-lab.herokuapp.com/",
+        methods: ["GET", "POST"]
+    }
+});
 const port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
